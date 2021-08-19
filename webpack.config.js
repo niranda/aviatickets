01cbbfca11bc0a24e0fdc2f7d6bcb4967b2ebdec
niranda <teslatorgames@gmail.com>
2021-08-19 14:00:47 +0300

    app build

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const autoprefixer = require("autoprefixer");
 const precss = require("precss");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -69,6 +70,7 @@ module.exports = {
   },
 
   plugins: [
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({ filename: "./style.css" }),
     new HtmlWebpackPlugin({
       template: "index.html",
